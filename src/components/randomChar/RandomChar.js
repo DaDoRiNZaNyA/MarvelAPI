7038ad8beac8b910e41e2513cd4acbc780ad810f
DaDoRiNZaNyA <123072513+DaDoRiNZaNyA@users.noreply.github.com>
2023-03-23 18:34:02 +0400

    error boundaries

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -30,12 +30,20 @@ class RandomChar extends Component {
         this.setState({char, loading: false})
     }
 
+    onCharLoading = () => {
+        this.setState({
+            char: {},
+            loading: true
+        })
+    }
+
     onError = () => {
         this.setState({loading: false, error: true})
     }
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+        this.onCharLoading();
         this.marvelService
             .getCharacter(id)
             .then(this.onCharLoaded)
