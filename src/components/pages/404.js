96fa947ef9242076f41570de9913e1f61220a94f
DaDoRiNZaNyA <123072513+DaDoRiNZaNyA@users.noreply.github.com>
2023-04-04 19:01:14 +0400

    meta & метод конечного автомата

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -1,9 +1,18 @@
+import { Helmet } from 'react-helmet';
+
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import { Link } from "react-router-dom";
 
 const Page404 = () => {
     return(
         <div>
+            <Helmet>
+                <meta
+                    name="404 Page"
+                    content="Error 404"
+                    /> 
+                <title>Error 404</title>
+            </Helmet>
             <ErrorMessage/>
             <p style={{'textAlign':'center', 'fontWeight': 'bold', 'fontSize': '24px'}}>Page doesn't exist</p>
             <Link style={{'display': 'block', 'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px'}} to="/">Back to main page</Link>
